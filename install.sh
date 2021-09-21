7839c851b55b316aef93d594aed2db6e278960ca
Ahmed Sabbour <asabbour@microsoft.com>
2021-09-21 14:35:49 -0700

    Updated install script

--- install.sh
+++ install.sh
@@ -2,6 +2,5 @@
 
 injected_dir=$1
 source /usr/local/s2i/install-common.sh
-install_deployments ${injected_dir}/injected-deployments.war
 install_modules ${injected_dir}/modules
 configure_drivers ${injected_dir}/drivers.env
